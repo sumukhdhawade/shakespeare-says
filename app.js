d64ab7dc7189181b5c4a74d0ae83c1622ed393e8
sumukhdhawade <sumukh.dhawade-coep@bvucoep.edu.in>
2021-10-15 12:36:29 +0530

    fix: made output box dynamic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#txt-output");
+var outputDiv = document.querySelector("#output-box");
 
 
 var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
